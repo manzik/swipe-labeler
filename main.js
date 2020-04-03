9acd09dfd74ef48a97d66d4327ef93462da91231
Mohse Yousefian <me@manzik.com>
2020-04-02 23:01:14 -0500

    Mark port number as optional argument

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ program
   .usage("[options]")
   .requiredOption("-d, --data <folderpath>", "folder path for data to label")
   .requiredOption("-s, --save <filepath>", "file path to save the resulting labels csv file")
-  .option("-p, --port <port>", "Port number", "8080")
   .requiredOption("-ll, --label-left <label>", "name for left swipe label")
   .requiredOption("-lr, --label-right <label>", "name for right swipe label")
+  .option("-p, --port [port]", "Port number", "8080")
   .option("-lu, --label-up [label]", "name for up swipe label")
   .option("-hc, --hide-class-numbers", "hide the number of labled and remaining items in client's browser", false);
 
